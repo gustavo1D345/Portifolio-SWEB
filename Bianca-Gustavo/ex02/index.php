bcd472f76f5843a44eca5f52d37a92fad0fb1f4f
Gusta <166881145+gustavo1D345@users.noreply.github.com>
2026-03-12 19:10:36 -0300

    Exercicio 02

---
+++ Bianca-Gustavo/ex02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercício</title>
+</head>
+<body>
+  
+<h2>Escreva a temperatura</h2>
+
+<form action="calcula.php" method="get">
+    <input type="number" name="F" placeholder="Temperatura em Fahrenheit" style="width:200px; height:auto;">
+    <input type="submit" value="Enviar">
+</form>
+
+<form action="calcula.php" method="get">
+    <input type="number" name="C" placeholder="Temperatura em Celsius" style="width:200px; height:auto;">
+    <input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
